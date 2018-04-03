1845ec8a1a80033389c4d945bc005be2e49562a2
Mattshu <mattshu67@gmail.com>
2018-04-02 23:20:54 -0400

    Reflow code layout

--- Archiver/ArchiverMainWindow.cs
+++ Archiver/ArchiverMainWindow.cs
@@ -7,44 +7,60 @@ using System.Windows.Forms;
 
 namespace Archiver {
     public partial class ArchiverMainWindow : Form {
-        private SortOrder sortOrder = SortOrder.Ascending;
-        private List<FileData> fileList = new List<FileData>();
+
         public SearchStyle searchStyle { get; private set; } = SearchStyle.ForFilesOlderThan;
         public SearchConditions searchConditions { get; private set; }
-        private string parentFolder;
 
-        private void BuildNewFileList() {
-            parentFolder = SetParentFolder();
-            if (parentFolder == null) return;
+        private List<FileData> fileList = new List<FileData>();
+        private string parentFolder;
+        private SortOrder sortOrder = SortOrder.Ascending;
+        
+        private void btnScan_Click(object sender, EventArgs e) {
+            BuildNewFileList();
+            LoadItemsFromFileList();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e) {
+            RefreshDataGridView();
+        }
+
+        private void dataGridView_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e) {
+            sortOrder = sortOrder == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+            var fileDataComparer = new FileDataComparer((ColumnType)e.ColumnIndex, sortOrder);
+            fileList.Sort(fileDataComparer);
+            dataGridView.Refresh();
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e) {
+            btnRefresh.Enabled = DataListHasItems();
+        }
+
+        private void radFilesOlderThan_CheckedChanged(object sender, EventArgs e) {
+            SetSearchStyle();
+            btnRefresh.Enabled = DataListHasItems();
+        }
+
+        private void contextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e) {
+            if (dataGridView.SelectedRows.Count <= 0) return;
+            var name = (string) dataGridView.SelectedRows[0].Cells["File"].Value;
+            var path = (string) dataGridView.SelectedRows[0].Cells["Path"].Value;
+            if (e.ClickedItem == ctxOpenFileLocation) {
+                Process.Start("explorer.exe", path);
+            }
+            else if (e.ClickedItem == ctxOpenFile) {
+                var pathToFile = path + "\\" + name;
+                Process.Start("explorer.exe", pathToFile);
+            }
+        }
+
+        private void contextMenu_Opening(object sender, CancelEventArgs e) {
+            e.Cancel = dataGridView.SelectedRows.Count <= 0;
+        }
+
+        private void RefreshDataGridView() {
             SetSearchConditions();
             BuildFileList();
-        }
-
-        private void BuildFileList() {
-            var getFileListForm = new GetFileListForm(parentFolder, searchConditions);
-            if (getFileListForm.ShowDialog() != DialogResult.OK || getFileListForm.fileList.Count <= 0) return;
-            fileList = getFileListForm.fileList;
-        }
-
-        private string SetParentFolder() {
-            folderBrowserDialog = new FolderBrowserDialog();
-            return folderBrowserDialog.ShowDialog() != DialogResult.OK ? null : folderBrowserDialog.SelectedPath;
-        }
-
-        private void SetSearchConditions() {
-            searchConditions = new SearchConditions {
-                Style = searchStyle,
-                Date = dateTimePicker.Value,
-                Option = chkIncludeSubDirs.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly
-            };
-        }
-
-        private void LoadItemsFromFileList() {
-            if (fileList.Count <= 0) return;
-            dataGridView.DataSource = null;
-            BuildDataGridViewColumns();
-            dataGridView.DataSource = fileList;
-            btnRefresh.Enabled = true; // TODO TEMP
+            LoadItemsFromFileList();
         }
 
         private void BuildDataGridViewColumns() {
@@ -89,56 +105,13 @@ namespace Archiver {
                 colDateCreated, colPath);
         }
 
-        private void btnScan_Click(object sender, EventArgs e) {
-            BuildNewFileList();
-            LoadItemsFromFileList();
-        }
-
-        private void btnRefresh_Click(object sender, EventArgs e) {
-            RefreshDataGridView();
-        }
-
-        private void contextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e) {
-            if (dataGridView.SelectedRows.Count <= 0) return;
-            var name = (string) dataGridView.SelectedRows[0].Cells["File"].Value;
-            var path = (string) dataGridView.SelectedRows[0].Cells["Path"].Value;
-            if (e.ClickedItem == ctxOpenFileLocation) {
-                Process.Start("explorer.exe", path);
-            }
-            else if (e.ClickedItem == ctxOpenFile) {
-                var pathToFile = path + "\\" + name;
-                Process.Start("explorer.exe", pathToFile);
-            }
-        }
-
-        private void dataGridView_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e) {
-            sortOrder = sortOrder == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
-            var fileDataComparer = new FileDataComparer((ColumnType) e.ColumnIndex, sortOrder);
-            fileList.Sort(fileDataComparer);
-            dataGridView.Refresh();
-        }
-
-        private void RefreshDataGridView() {
-            SetSearchConditions();
-            BuildFileList();
-            LoadItemsFromFileList();
-        }
-
-        private void contextMenu_Opening(object sender, CancelEventArgs e) {
-            e.Cancel = dataGridView.SelectedRows.Count <= 0;
-        }
-
-        private void dateTimePicker_ValueChanged(object sender, EventArgs e) {
-            btnRefresh.Enabled = ListHasItems();
-        }
-
-        private bool ListHasItems() {
+        private bool DataListHasItems() {
             return dataGridView.RowCount > 0;
         }
 
-        private void radFilesOlderThan_CheckedChanged(object sender, EventArgs e) {
-            SetSearchStyle();
-            btnRefresh.Enabled = ListHasItems();
+        private string SetParentFolder() {
+            folderBrowserDialog = new FolderBrowserDialog();
+            return folderBrowserDialog.ShowDialog() != DialogResult.OK ? null : folderBrowserDialog.SelectedPath;
         }
 
         private void SetSearchStyle() {
@@ -147,8 +120,38 @@ namespace Archiver {
                 : SearchStyle.ForFilesUntouchedSince;
         }
 
+        private void SetSearchConditions() {
+            searchConditions = new SearchConditions {
+                Style = searchStyle,
+                Date = dateTimePicker.Value,
+                Option = chkIncludeSubDirs.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly
+            };
+        }
+
+        private void BuildNewFileList() {
+            parentFolder = SetParentFolder();
+            if (parentFolder == null) return;
+            SetSearchConditions();
+            BuildFileList();
+        }
+
+        private void BuildFileList() {
+            var getFileListForm = new GetFileListForm(parentFolder, searchConditions);
+            if (getFileListForm.ShowDialog() != DialogResult.OK || getFileListForm.fileList.Count <= 0) return;
+            fileList = getFileListForm.fileList;
+        }
+
+        private void LoadItemsFromFileList() {
+            if (fileList.Count <= 0) return;
+            dataGridView.DataSource = null;
+            BuildDataGridViewColumns();
+            dataGridView.DataSource = fileList;
+            btnRefresh.Enabled = true; // TODO TEMP
+        }
+
         public ArchiverMainWindow() {
             InitializeComponent();
         }
+
     }
 }
